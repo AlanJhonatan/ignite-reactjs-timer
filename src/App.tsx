687979ef9f8dba21d716398a763f3398265e4251
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-13 01:28:50 +0200

    feat: add some styled components styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+    </>
   )
 }
