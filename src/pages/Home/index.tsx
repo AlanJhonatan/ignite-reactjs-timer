23a6dff874d07ace2f0649885003e28c09ac589b
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-15 22:22:26 +0200

    feat: add date-fns to handle timer countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,8 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -25,6 +26,7 @@ interface Cycle {
   id: string
   task: string
   minutes: number
+  startDate: Date
 }
 
 export function Home() {
@@ -40,6 +42,18 @@ export function Home() {
     },
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
 
@@ -47,6 +61,7 @@ export function Home() {
       id,
       task: data.task,
       minutes: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
@@ -54,8 +69,6 @@ export function Home() {
 
     reset()
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutes * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
